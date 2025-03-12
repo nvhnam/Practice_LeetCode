12f85419003cfa1ab919f5241a7ce45c80fc7795
nvhnam <nvhnam01@gmail.com>
2025-03-12 18:25:50 +0700

    Remove number that has more than 2 duplicates in sorted array and return its length.

--- Problems/remove_duplicate_from_sorted_array.py
+++ Problems/remove_duplicate_from_sorted_array.py
@@ -10,3 +10,14 @@ class RemoveDuplicateFromSortedArray:
                 k += 1
         print(nums)
         return k
+    
+    def removeDuplicates2(self, nums: List[int]) -> int:
+        k = 2
+        for i in range(2, len(nums)):
+            if(nums[i] != nums[k - 2]):
+                nums[k] = nums[i]
+                k += 1
+
+        print(nums)
+        return k
+            
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Problems import MaxProfit, MergeSortedArrays, RemoveElements, RemoveDuplica
 
 def main():
     removeDuplicate = RemoveDuplicateFromSortedArray()
-    nums = [0,0,1,1,1,2,2,3,3,4]
-    print(removeDuplicate.removeDuplicates(nums))
+    nums = [0,0,1,1,1,1,2,3,3]
+    print(removeDuplicate.removeDuplicates2(nums))
 
 main()
