8ea12227f3e26f7d748d76c3dc5a9be6b492c69d
nvhnam <nvhnam01@gmail.com>
2025-03-20 13:30:59 +0700

    Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.

--- Problems/three_sum.py
+++ Problems/three_sum.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 
 class ThreeSum:
@@ -23,3 +23,23 @@ class TwoSum:
                 return res
 
         return res
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right    
+
+class TwoSumIV:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        l = set()
+        def dfs(node):
+            if not node:
+                return False
+            y = k - node.val
+            if y in l:
+                return True
+            else:
+                l.add(node.val)
+                return (dfs(node.left) or dfs(node.right))
+        return dfs(root)
