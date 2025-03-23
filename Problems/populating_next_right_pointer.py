00d763583250e388a5bee676e23a20e0c7ba5360
nvhnam <nvhnam01@gmail.com>
2025-03-23 21:10:04 +0700

    Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.

--- Problems/populating_next_right_pointer.py
+++ Problems/populating_next_right_pointer.py
@@ -10,4 +10,14 @@ class Node:
 
 class PopulatingNextRightPointer:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        pass
+        cur, nxt = root, root.left if root else None
+
+        while cur and nxt:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+            cur = cur.next
+            if not cur:
+                cur = nxt
+                nxt = cur.left
+        return root
