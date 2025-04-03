7035f4603ad0d288fdbc92b2a7788894582e9a8b
nvhnam <nvhnam01@gmail.com>
2025-04-03 14:21:53 +0700

    Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.

---
+++ Problems/searchBST.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class SearchBST:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root: return None
+        if root.val == val: return root
+
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        else:
+            return self.searchBST(root.right, val)
