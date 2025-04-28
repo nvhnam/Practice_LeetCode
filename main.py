f62bbee5ecd6ea62448a3be54928e5f575c7fbe1
nvhnam <nvhnam01@gmail.com>
2025-04-28 22:06:53 +0700

    Given the root of a binary tree, return the inorder traversal of its nodes' values.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from Problems import MaxProfit, MergeSortedArrays, RemoveElements, RemoveDuplicateFromSortedArray, MajorityElement, KidsWithCandies, CanPlaceFlowers, ReverseVowels, SmallestInfiniteSet, AsteroidCollision, BinaryGap, SearchInsertPosition, MissingNumber, CountTreeNodes, TreeNode, DiagonalTraverse, DesignHashmap, DesignHashSet, PathSum, TreeNode2, AddingNegaBinaryNumber, AddTwoNumbers, ThreeSum, TwoSum, LRUCache, QueueUsingStack, SubArraySumK, MostCommmonWord, PopulatingNextRightPointer, CustomSortString, SpiralMatrix, SwappingNodesLinkedList, ValidParentheses, ValidPalindromeII, ReverseWordsInString, KLargestInArray, DifferenceTwoArrays, MaxAvgSubArray, UniqueNumberOccur, MaxDepthBinary, ContainerMaxWater, NTribonaccyNum, RemovingStarFromString, IncreasingTripletSubsequence, ProductArrayExceptSelf, MaxVowelInSubString, MaxConsecutiveOneIII, DecodeString, IntToRoman, RomanToInt
+from Problems import MaxProfit, MergeSortedArrays, RemoveElements, RemoveDuplicateFromSortedArray, MajorityElement, KidsWithCandies, CanPlaceFlowers, ReverseVowels, SmallestInfiniteSet, AsteroidCollision, BinaryGap, SearchInsertPosition, MissingNumber, CountTreeNodes, TreeNode, DiagonalTraverse, DesignHashmap, DesignHashSet, PathSum, TreeNode2, AddingNegaBinaryNumber, AddTwoNumbers, ThreeSum, TwoSum, LRUCache, QueueUsingStack, SubArraySumK, MostCommmonWord, PopulatingNextRightPointer, CustomSortString, SpiralMatrix, SwappingNodesLinkedList, ValidParentheses, ValidPalindromeII, ReverseWordsInString, KLargestInArray, DifferenceTwoArrays, MaxAvgSubArray, UniqueNumberOccur, MaxDepthBinary, ContainerMaxWater, NTribonaccyNum, RemovingStarFromString, IncreasingTripletSubsequence, ProductArrayExceptSelf, MaxVowelInSubString, MaxConsecutiveOneIII, DecodeString, IntToRoman, RomanToInt, TreeTraversal
 
 def main():
-    roman_to_int = RomanToInt()
-    s = "LVIII"
-    print(roman_to_int.romanToInt(s))
+    tree_traversal = TreeTraversal()
+    root = [1,None,2,3]
+    print(tree_traversal.inorderTraversal(root))
 
 main()
