42f84e68682f9d684656837ad6ce512de0d654cc
nvhnam <nvhnam01@gmail.com>
2025-05-09 14:10:48 +0700

    All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.

--- Problems/isomorphic_string.py
+++ Problems/isomorphic_string.py
@@ -1,3 +1,9 @@
 class IsomorphicString:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        pass
+        dict1, dict2 = {}, {}
+        for s1, t1 in zip(s, t):
+            if (s1 in dict1 and dict1[s1] != t1) or (t1 in dict2 and dict2[t1] != s1):
+                return False
+            dict1[s1] = t1
+            dict2[t1] = s1
+        return True
