8fb7f79bc508becc5afcd794f5d26d07d999ac3b
nvhnam <nvhnam01@gmail.com>
2025-05-09 14:14:47 +0700

    Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.

--- Problems/intersection_two_linkedlist.py
+++ Problems/intersection_two_linkedlist.py
@@ -7,4 +7,8 @@ class ListNode:
 
 class IntersectionOfTwoLinkedList:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        pass
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
