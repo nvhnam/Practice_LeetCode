8a59489e7634323905ad8e6e96cab99470ee37aa
nvhnam <nvhnam01@gmail.com>
2025-05-09 14:47:32 +0700

    Update questions

---
+++ Problems/intersection_two_arrays.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class IntersectionTwoArrays:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
+
+
+class IntersectionTwoArraysII:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        pass
