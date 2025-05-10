106d0e89f1a69af0ad744208ca37b4431d559edc
nvhnam <nvhnam01@gmail.com>
2025-05-10 10:08:52 +0700

    Given two integer arrays nums1 and nums2, return an array of their intersection

--- Problems/intersection_two_arrays.py
+++ Problems/intersection_two_arrays.py
@@ -1,11 +1,19 @@
+from collections import Counter
 from typing import List
 
 
 class IntersectionTwoArrays:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        n1, n2 = set(nums1), set(nums2)
+        return list(n1 & n2)
 
 
 class IntersectionTwoArraysII:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        pass
+        c = Counter(nums1)
+        res = []
+        for n in nums2:
+            if c[n] > 0:
+                res.append(n)
+                c[n] -= 1
+        return res
