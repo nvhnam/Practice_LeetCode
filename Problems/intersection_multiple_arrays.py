7d7ecaf3dd395c9ac740e80a8c52e94bd7ed8127
nvhnam <nvhnam01@gmail.com>
2025-05-12 12:53:44 +0700

    Given a 2D integer array nums where nums[i] is a non-empty array of distinct positive integers, return the list of integers that are present in each array of nums sorted in ascending order.

--- Problems/intersection_multiple_arrays.py
+++ Problems/intersection_multiple_arrays.py
@@ -1,6 +1,18 @@
+from collections import Counter
 from typing import List
 
 
 class IntersectionMultipleArrays:
     def intersection(self, nums: List[List[int]]) -> List[int]:
-        pass
+        c = Counter(nums[0])
+
+        for i in range(1, len(nums)):
+            for n in nums[i]:
+                if n in c:
+                    c[n] += 1
+
+        res = []
+        for n in c.keys():
+            if c[n] == len(nums):
+                res.append(n)
+        return sorted(res)
