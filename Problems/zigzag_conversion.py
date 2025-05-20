55309e97dc00bf71f24956c248bcb60fc87e3353
nvhnam <nvhnam01@gmail.com>
2025-05-20 20:56:56 +0700

    Write the code that will take a string and make this conversion given a number of rows

--- Problems/zigzag_conversion.py
+++ Problems/zigzag_conversion.py
@@ -1,3 +1,18 @@
 class ZigzagConversion:
     def convert(self, s: str, numRows: int) -> str:
-        pass
+        if numRows == 1 or len(s) <= numRows:
+            return s
+        rows = [[] for _ in range(numRows)]
+        idx, d = 0, 1
+
+        for char in s:
+            rows[idx].append(char)
+            if idx == 0:
+                d = 1
+            elif idx == numRows - 1:
+                d = -1
+            idx += d
+        
+        for i in range(numRows):
+            rows[i] = "".join(rows[i])
+        return "".join(rows)
